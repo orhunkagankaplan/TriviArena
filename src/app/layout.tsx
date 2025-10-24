03ba510f9d8d4d482fe6d5c8316ed79b4027f0b5
orhunkagankaplan <126966158+orhunkagankaplan@users.noreply.github.com>
2025-10-24 20:57:29 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,8 @@
-import "./globals.css";
+import './globals.css';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return <html lang="en"><body>{children}</body></html>;
+}
 // app/layout.tsx veya src/app/layout.tsx
 
 export const metadata = {
